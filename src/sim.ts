25fb1f5709dcc36035ad1e4b7fe89c57fa4a0020
Luca <luca-c@hotmail.de>
2021-03-24 07:12:06 +0100

    Partially implemented simulated annealing

--- src/sim.ts
+++ src/sim.ts
@@ -1,22 +1,12 @@
-const repulsion = 4000;
-const stiffness = 400;
-
-const attraction = 400;
-const damping = 0.5;
-const wiggle = 1000;
-
-const dt = 1 / 1000;
-const steps = 100000;
-
 function dist(a: Point, b: Point) {
-    return Math.sqrt((a.x - b.x) ** 2 + (a.y - b.y) ** 2);
+    const dx = a.x - b.x;
+    const dy = (a.y - b.y) * 2; // you counts double cause 2 heightl
+
+    return Math.sqrt(dx * dx + dy * dy);
 }
 
-function collides(a: Point, b: Point) {
-    return a.x < b.x + b.width &&
-        a.x + a.width > b.x &&
-        a.y < b.y + b.height &&
-        a.y + a.height > b.y;
+function rand(min: number, max: number) {
+    return (Math.random() * (max - min)) + min;
 }
 
 class Point {
@@ -24,58 +14,24 @@ class Point {
     x = 0;
     y = 0;
 
-    // Size
-    width: number;
-    height: number;
+    id: number;
 
-    // Velocity
-    vx = 0;
-    vy = 0;
+    fixed: boolean;
+    connected: Point[] = [];
 
-    // Acceleration
-    ax = 0;
-    ay = 0;
+    constructor(id: number, fixed: boolean) {
+        this.id = id;
+        this.fixed = fixed;
+    }
 
-    fixed = false;
-
-    updatePosition(collidesAny?) {
-        this.vx = (this.vx + this.ax * dt) * damping;
-        this.vy = (this.vy + this.ay * dt) * damping;
-
-        if (!this.fixed) {
-            let ox = this.x;
-            let oy = this.y;
-
-            this.x += this.vx * dt;
-            this.y += this.vy * dt;
-
-            this.x = this.x;
-            this.y = Math.max(this.y, 2);
-
-            if (collidesAny) {
-                let o = collidesAny(this);
-                if (o) {
-                    /*o.ax += this.ax / 2;
-                    o.ay += this.ay / 2;*/
-
-                    this.y = oy;
-                    if (collidesAny(this)) {
-                        this.x = ox;
-                        this.y = Math.max(oy + this.vy * dt, 2);
-                        if (collidesAny(this)) {
-                            this.y = oy;
-                        }
-                    }
-                }
-            }
+    getMidPoint() {
+        let mx = 0;
+        let my = 0;
+        for (const o of this.connected) {
+            mx += o.x;
+            my += o.y;
         }
-
-        this.ax = (Math.random() - 0.5) * wiggle;
-        this.ay = (Math.random() - 0.5) * wiggle;
-
-        if (isNaN(this.x) || isNaN(this.y)) {
-            debugger;
-        }
+        return [mx / this.connected.length, my / this.connected.length];
     }
 }
 
@@ -85,18 +41,19 @@ interface Edge {
 }
 
 class Simulator {
-    nodes: Point[] = [];
+    public nodes: Point[] = [];
     private edges: Edge[] = [];
 
-    addNode(x = 0, y = 0, width = 1, height = 1, fixed = false) {
-        let n = new Point();
+    private compactionDir: boolean;
+    private iterationCount: number;
+    private T: number;
+    private k: number;
 
-        n.x = x;
-        n.y = y;
-        n.width = width;
-        n.height = height;
-        n.fixed = fixed;
+    private gridSize: number;
+    private grid: Point[];
 
+    addNode(fixed: boolean) {
+        let n = new Point(this.nodes.length, fixed);
         return this.nodes.push(n) - 1;
     }
 
@@ -109,134 +66,136 @@ class Simulator {
 
         if (this.edges.some(x => x.a == an && x.b == bn || x.b == an && x.a == bn)) return;
 
+        an.connected.push(bn);
+        bn.connected.push(an);
+
         this.edges.push({
             a: an,
             b: bn
         });
     }
 
-    sim() {
-        let changed = true;
-        while (changed) {
-            changed = false;
+    private delEdge(e: Edge) {
 
-            // normal graph layout
-            for (let i = 0; i < steps; i++) {
-                this.step1();
+    }
+
+    sim(errorCallback: (a: number, b: number) => void) {
+        let run = true;
+        while (run) {
+            run = false;
+            this.reset();
+
+            for (let i = 0; i < this.iterationCount; i++) {
+                this.simStep();
             }
 
-            { 
-                // align to grid
-                for (const n of this.nodes) {
-                    n.x = Math.floor(n.x);
-                    n.y = Math.floor(n.y);
-                }
+            for (const e of this.edges) {
+                let len = dist(e.a, e.b);
 
-                // resolve collisions
-                let changed = true;
-                while (changed) {
-                    changed = false;
-                    for (const n of this.nodes) {
-                        if (this.collidesAny(n)) {
-                            n.x++;
-                            changed = true;
-                        }
-                    }
-                }
-            }
+                if (len > 9) {
+                    errorCallback(e.a.id, e.b.id);
+                    /* causes infinite loop
+                    this.delEdge(e);
 
-            // pull everything together
-            for (let i = 0; i < steps; i++) {
-                this.step2();
-            }
+                    let p = this.addNode(false);
+                    this.addEdge(e.a.id, p);
+                    this.addEdge(p, e.b.id);
 
-            for (const c of this.edges) {
-                let d = dist(c.a, c.b);
-                if (d > 9) {
-                    // TODO: split edge
-                    // throw new Error("Not implemented");
-                    // changed = true;
+                    run = true;*/
                 }
             }
         }
     }
 
-    private collidesAny(a: Point) {
-        for (const b of this.nodes) {
-            if (a == b) continue;
+    private initBFS() {
+        let next = [];
 
-            if (collides(a, b)) {
-                return b;
-            }
-        }
-
-        return null;
-    }
-
-    private step1() {
-        // move away from everyone
-        for (const a of this.nodes) {
-            for (const b of this.nodes) {
-                let dx = a.x - b.x;
-                let dy = a.y - b.y;
-
-                if (Math.abs(dx) < 1 || Math.abs(dy) < 2) {
-                    // distance squared, add to avoid / 0
-                    let dist = dx * dx + dy * dy;
-
-                    dist = repulsion / (dist + 0.01);
-                    a.ax += dx * dist;
-                    a.ay += dy * dist;
-                }
-            }
-        }
-
-        // spring to connections
-        for (const c of this.edges) {
-            let d = (3 - dist(c.a, c.b)) * stiffness;
-            let dx = (c.a.x - c.b.x) * d;
-            let dy = (c.a.y - c.b.y) * d;
-
-            c.a.ax += dx;
-            c.a.ay += dy;
-
-            c.b.ax -= dx;
-            c.b.ay -= dy;
-        }
-
+        let x = Math.floor(this.gridSize / 2);
         for (const n of this.nodes) {
-            n.updatePosition();
+            if (n.fixed) {
+                n.x = x++;
+
+                for (const o of n.connected) {
+                    next.push(o);
+                }
+            } else {
+                n.x = -1;
+            }
+            n.y = -1;
+        }
+
+        let y = 0;
+        while (next.length > 0) {
+            let temp = [];
+            x = 0;
+            for (const n of next) {
+                if (n.x != -1) continue;
+
+                n.x = x;
+                n.y = y;
+                this.setNode(x, y, n);
+
+                for (const asdf of n.connected) {
+                    temp.push(asdf);
+                }
+
+                x++;
+            }
+            next = temp;
+            y++;
         }
     }
 
-    private step2() {
-        for (const a of this.nodes) {
-            // attract to center
-            a.ax -= a.x * 100;
-            a.ay -= 100;
-        }
+    private reset() {
+        this.gridSize = Math.floor(5 * Math.sqrt(this.nodes.length));
+        this.grid = new Array(this.gridSize * this.gridSize);
+        this.initBFS();
 
-        // pull to connections
-        for (const c of this.edges) {
-            let dx = c.b.x - c.a.x;
-            let dy = c.b.y - c.a.y;
+        /*for (const n of this.nodes) {
+            n.cost = n.getCost();
+        }*/
 
-            let dist = (dx * dx + dy * dy) / 100 * attraction;
+        this.compactionDir = true;
+        this.iterationCount = 2000 * Math.sqrt(this.nodes.length);
+        this.T = 2 * Math.sqrt(this.nodes.length);
+        this.k = (0.2 / this.T) ** (1 / this.iterationCount);
+    }
 
-            dx *= dist;
-            dy *= dist;
-
-            c.a.ax += dx;
-            c.a.ay += dy;
-
-            c.b.ax -= dx;
-            c.b.ay -= dy;
-        }
-
-        let f = this.collidesAny.bind(this)
+    private simStep() {
         for (const n of this.nodes) {
-            n.updatePosition(f);
+            if (n.fixed) continue;
+
+            // find optimal place to put node
+
+            const [mx, my] = n.getMidPoint();
+            const fx = Math.round(mx + rand(-this.T, this.T));
+            const fy = Math.round(my + rand(-this.T, this.T));
+
+            if (fx < 0 || fx >= this.gridSize || fy < 0 || fy >= this.gridSize)
+                continue;
+
+            if (n.x == fx && n.y == fy) {
+                // swapNeighbor(n);
+            } else {
+                if (!this.getNode(fx, fy)) {
+                    this.setNode(n.x, n.y, null);
+                    this.setNode(fx, fy, n);
+                    n.x = fx;
+                    n.y = fy;
+                } else {
+                    // insertNear(n, fx, fy);
+                }
+            }
         }
+
+        this.T *= this.k;
+    }
+
+    private getNode(x: number, y: number) {
+        return this.grid[x + y * this.gridSize];
+    }
+    private setNode(x: number, y: number, v: Point) {
+        this.grid[x + y * this.gridSize] = v;
     }
 }
 
