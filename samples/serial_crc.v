4849baaedd8eaaca4667c4a22ceef6afe5daaf8c
Luca <luca-c@hotmail.de>
2021-03-28 16:22:20 +0200

    Updated samples

--- samples/serial_crc.v
+++ samples/serial_crc.v
@@ -1,48 +1,17 @@
-//-----------------------------------------------------
-// Design Name : serial_crc_ccitt
-// File Name   : serial_crc.v
-// Function    : CCITT Serial CRC
-// Coder       : Deepak Kumar Tala
-//-----------------------------------------------------
-module serial_crc_ccitt (
+module serial_crc (
     input clk,
     input reset,
     input enable,
-    input init,
     input data_in,
-    output [15:0] crc_out);
+    output reg [15:0] crc);
 
-    //------------Internal Variables--------
-    reg   [15:0] lfsr;
+    parameter initVal = 16'hFFFF;
+    parameter poly = 16'h1021;
 
-    //-------------Code Start-----------------
-    assign crc_out = lfsr;
-
-    // Logic to CRC Calculation
     always @(posedge clk)
-        if (reset) begin
-            lfsr <= 16'hFFFF;
-        end else if (enable) begin
-            if (init) begin
-                lfsr <=  16'hFFFF;
-            end else begin
-                lfsr[0]  <= data_in ^ lfsr[15];
-                lfsr[1]  <= lfsr[0];
-                lfsr[2]  <= lfsr[1];
-                lfsr[3]  <= lfsr[2];
-                lfsr[4]  <= lfsr[3];
-                lfsr[5]  <= lfsr[4] ^ data_in ^ lfsr[15];
-                lfsr[6]  <= lfsr[5];
-                lfsr[7]  <= lfsr[6];
-                lfsr[8]  <= lfsr[7];
-                lfsr[9]  <= lfsr[8];
-                lfsr[10] <= lfsr[9];
-                lfsr[11] <= lfsr[10];
-                lfsr[12] <= lfsr[11] ^ data_in ^ lfsr[15];
-                lfsr[13] <= lfsr[12];
-                lfsr[14] <= lfsr[13];
-                lfsr[15] <= lfsr[14];
-            end
-        end
+        if (reset)
+            crc <= initVal;
+        else if (enable)
+            crc <= (crc << 1) ^ ((data_in ^ crc[15]) * poly);
 
 endmodule
