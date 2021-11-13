eced26909f73d4716d1565c48304c20b15eb5e3e
Redcrafter <redcrafter@hotmail.de>
2021-11-13 20:25:19 +0100

    moved options parsing back to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,70 @@
 import fs from "fs";
+import { Command } from "commander";
+import readline from 'readline';
 
 import { Blueprint, createBlueprint, createBpString } from "./blueprint.js";
 import { logger } from "./logger.js";
-import { options, parseOptions } from './options.js';
+import { options } from './options.js';
 import { buildGraph } from "./parser.js";
 import { transform } from "./transformer.js";
 import { genNetlist } from "./yosys.js";
 
-await parseOptions();
+{ // parse command line options
+    const program = new Command("v2f");
+
+    program
+        .arguments("<files..>")
+        .helpOption("-h, --help", "Display this information.")
+        .option("-v, --verbose")
+        // .option("-s, --seed <seed>", "Specify a seed for the layout generation.")
+        .option("-o, --output <file>", "File to output the compiled blueprint to.")
+        .option("-m, --modules <names...>", "Verilog modules to output blueprint for. (defaults to all).")
+        .option("-f, --files <files...>", "List of Verilog files to compile. (only has to be explicitly specified after -m).")
+        .option("-r, --retry", "Retry until there are no longer layout errors.")
+        .option("-g, --generator [type]", "Layout generator to use. annealing(default),matrix");
+
+    program.parse();
+
+    let _options = program.opts();
+
+    for (const key in _options) {
+        // @ts-ignore
+        options[key] = _options[key];
+    }
+
+    // merge default and file options
+    options.files = options.files ?? [];
+    options.files.push(...program.args);
+
+    if (options.files.length == 0) {
+        logger.log("error: no input files");
+        if (options.modules) {
+            logger.log("did you forget -f for files?");
+        }
+        process.exit(0);
+    }
+
+    if (options.output) {
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+
+        if (fs.existsSync(options.output)) {
+            let res = await new Promise<string>(res => rl.question(`${options.output} already exists. Overwrite? [y/n] `, res));
+            if (res.toLowerCase() !== "y") {
+                process.exit(0);
+            }
+        }
+
+        rl.close();
+    }
+
+    if (options.generator != "annealing" && options.generator != "matrix") {
+        logger.log(`Unknown layout generator: ${options.generator}`);
+        process.exit(0);
+    }
+}
 
 const data = await genNetlist(options.files);
 const modules: Blueprint[] = [];
--- src/options.ts
+++ src/options.ts
@@ -1,8 +1,3 @@
-import { Command } from "commander";
-import fs from "fs";
-import readline from 'readline';
-import { logger } from "./logger.js";
-
 export let options: {
     verbose: boolean;
     // seed?: string;
@@ -15,61 +10,3 @@ export let options: {
     verbose: false,
     generator: "annealing"
 };
-
-export async function parseOptions() {
-    const program = new Command("v2f");
-
-    program
-        .arguments("<files..>")
-        .helpOption("-h, --help", "Display this information.")
-        .option("-v, --verbose")
-        // .option("-s, --seed <seed>", "Specify a seed for the layout generation.")
-        .option("-o, --output <file>", "File to output the compiled blueprint to.")
-        .option("-m, --modules <names...>", "Verilog modules to output blueprint for. (defaults to all).")
-        .option("-f, --files <files...>", "List of Verilog files to compile. (only has to be explicitly specified after -m).")
-        .option("-r, --retry", "Retry until there are no longer layout errors.")
-        .option("-g, --generator [type]", "Layout generator to use. annealing(default),matrix");
-
-    program.parse();
-
-    let _options = program.opts();
-
-    for (const key in _options) {
-        // @ts-ignore
-        options[key] = _options[key];
-    }
-
-    // merge default and file options
-    options.files = options.files ?? [];
-    options.files.push(...program.args);
-
-    if (options.files.length == 0) {
-        logger.log("error: no input files");
-        if (options.modules) {
-            logger.log("did you forget -f for files?");
-        }
-        process.exit(0);
-    }
-
-    if (options.output) {
-        const rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout
-        });
-
-        if (fs.existsSync(options.output)) {
-            let res = await new Promise<string>(res => rl.question(`${options.output} already exists. Overwrite? [y/n] `, res));
-            if (res.toLowerCase() !== "y") {
-                process.exit(0);
-            }
-        }
-
-        rl.close();
-    }
-
-    if (options.generator != "annealing" && options.generator != "matrix") {
-        logger.log(`Unknown layout generator: ${options.generator}`);
-        process.exit(0);
-    }
-}
-
